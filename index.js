f34822434f9c5a59fed53796b6efcbfdf8f9f0ff
Kendra Allen <kallen5@ufl.edu>
2025-05-25 14:23:20 -0400

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code here
+function greet() {}
+//prompt function asking user's name
+let userName = prompt("What is your name?");
+if (userName)  {
+    alert('Hello ' + userName);
+}
+//prompt asking user age
+let userAge = prompt("How old are you?");
